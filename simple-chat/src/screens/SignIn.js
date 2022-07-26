e925dc416e4914c021099c343ae71c86fbe8f292
dailyjudge <jmk961202@gmail.com>
2022-07-26 22:54:06 +0900

    chat-login-page-commit

--- simple-chat/src/screens/SignIn.js
+++ simple-chat/src/screens/SignIn.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext, useRef, useState } from "react";
+import { ThemeContext } from "styled-components/native";
 import styled from "styled-components/native";
-import { Button } from "react-native";
+import { Button, Image, Input } from "../components";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Container = styled.View`
@@ -17,17 +18,50 @@ const StyleText = styled.Text`
   font-size: 30px;
   color: #111111;
 `;
+const Logo =
+  "https://firebasestorage.googleapis.com/v0/b/rn-chat-7f25d.appspot.com/o/logo.png?alt=media";
 
 const SignIn = ({ navigation }) => {
   const insets = useSafeAreaInsets();
+  const theme = useContext(ThemeContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const refPassword = useRef(null);
+
+  const _handleSigninBtnPress = () => {
+    console.log("signin");
+  };
   return (
     <Container insets={insets}>
-      <StyleText>Sign in</StyleText>
+      <Image url={Logo} />
+
+      <Input
+        label="Email"
+        placeholder="Email"
+        returnKeyType="next"
+        value={email}
+        onChangeText={setEmail}
+        onSubmitEditing={() => refPassword.current.focus()}
+      />
+      <Input
+        ref={refPassword}
+        label="Password"
+        placeholder="Password"
+        returnKeyType="done"
+        value={password}
+        onChangeText={setPassword}
+        isPassword={true}
+        onSubmitEditing={_handleSigninBtnPress}
+      />
+      <Button title="Sign in" onPress={_handleSigninBtnPress} />
       <Button
-        title="Sign up"
+        title="or Sign up"
         onPress={() => {
           navigation.navigate("SignUp");
         }}
+        containerStyle={{ marginTop: 0, backgroundColor: "transparent" }}
+        textStyle={{ color: theme.btnTextLink, fontSize: 18 }}
       />
     </Container>
   );
